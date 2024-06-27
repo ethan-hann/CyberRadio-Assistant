a7463315d31190cdd84cec68b5c742fb54b72b06
Ethan Hann <36464732+ethan-hann@users.noreply.github.com>
2024-06-27 18:24:41 -0500

    Update Updater.cs
    Fix typo in latest version and current version comparison.

--- CyberRadio.Code/utility/Updater.cs
+++ CyberRadio.Code/utility/Updater.cs
@@ -27,7 +27,7 @@ namespace RadioExt_Helper.utility
                     //Strip the build from the version number
                     currentVersion = new Version(currentVersion.Major, currentVersion.Minor, currentVersion.Build);
 
-                    if (latestVersion == currentVersion)
+                    if (latestVersion > currentVersion)
                     {
                         var text = GlobalData.Strings.GetString("UpdateAvailableNotice");
                         var caption = GlobalData.Strings.GetString("UpdateAvailable");
