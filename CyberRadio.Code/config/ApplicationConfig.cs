cca297a2b50fdca5effc4c7bb3d6be961e034d2a
Ethan Hann <36464732+ethan-hann@users.noreply.github.com>
2024-07-09 19:36:57 -0500

    Updated ApplicationConfig and CyberConfig class
    - Modified namespace and added using directives in ApplicationConfig.cs for improved organization.
    - Updated CyberConfig class summary to clarify its role in configuration management.
    - Set default value of AutoExportToGame in CyberConfig to false, specifying default behavior.
    - Various code fixes and improvements including adding comments and fixing logic errors in multiple classes.
    - Addition of a license header to each file in the project.

--- CyberRadio.Code/config/ApplicationConfig.cs
+++ CyberRadio.Code/config/ApplicationConfig.cs
@@ -1,3 +1,19 @@
+// CyberConfig.cs : RadioExt-Helper
+// Copyright (C) 2024  Ethan Hann
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 using AetherUtils.Core.Attributes;
 using AetherUtils.Core.Configuration;
 using RadioExt_Helper.utility;
@@ -5,7 +21,8 @@ using RadioExt_Helper.utility;
 namespace RadioExt_Helper.config;
 
 /// <summary>
-///     A DTO representing the configuration for the application; replaces the need for a settings file. All present and future configuration options
+///     A DTO representing the configuration for the application; replaces the need for a settings file. All present and
+///     future configuration options
 ///     will be done via this class.
 /// </summary>
 public sealed class CyberConfig
@@ -21,7 +38,7 @@ public sealed class CyberConfig
     ///     staging.
     /// </summary>
     [Config("autoExportToGame")]
-    public bool AutoExportToGame { get; set; }
+    public bool AutoExportToGame { get; set; } = false;
 
     /// <summary>
     ///     The path to the staging directory.
